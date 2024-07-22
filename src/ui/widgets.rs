f5ad40407c5a69fddd3b5419a49e84dbece45c1c
unknown <152326772+kainosaur@users.noreply.github.com>
2024-07-22 08:47:38 -0600

    widget work

--- src/ui/widgets.rs
+++ src/ui/widgets.rs
@@ -22,8 +22,8 @@ impl<T: Spawn> Widgets for T {
             Name::new("Button"),
             ButtonBundle {
                 style: Style {
-                    width: Px(200.0),
-                    height: Px(65.0),
+                    width: Percent(40.0),
+                    height: Percent(20.0),
                     justify_content: JustifyContent::Center,
                     align_items: AlignItems::Center,
                     ..default()
